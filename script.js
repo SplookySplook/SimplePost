0162187c8fd8a35068f7227ab1f8f3476354e7d0
SplookySplook <alvosgien@gmail.com>
2026-07-24 19:40:49 -0700

    Please pray for me that this will work

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ function renderUI() {
                 <svg viewBox="0 0 24 24"><path d="M2.01 21L23 12 2.01 3 2 10l15 2-15 2z"/></svg>
                 Send
             </button>
-        `
+        `;
         addingContainer.appendChild(box);
 
         document.getElementById('send').addEventListener('click', async () => {
